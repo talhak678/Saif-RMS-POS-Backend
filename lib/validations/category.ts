eee544c1433d2af5cc8a65dd4c0a2b97043210a0
huzaifa-codes <huzaifabusiness60@gmail.com>
2026-02-27 22:12:57 +0500

    chnages updatd

--- lib/validations/category.ts
+++ lib/validations/category.ts
@@ -3,5 +3,6 @@ import { z } from 'zod'
 export const categorySchema = z.object({
     name: z.string().min(2, 'Name must be at least 2 characters'),
     description: z.string().optional(),
+    image: z.string().optional(),
     restaurantId: z.string().cuid('Invalid restaurant ID'),
 })
