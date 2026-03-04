2b3038dcbb6cd6940155b416f465af5cc9f01132
huzaifa-codes <huzaifabusiness60@gmail.com>
2026-03-05 00:00:01 +0500

    chnages updated

--- app/api/orders/[id]/route.ts
+++ app/api/orders/[id]/route.ts
@@ -1,4 +1,4 @@
-import prisma from '@/lib/prisma'
+    import prisma from '@/lib/prisma'
 import { successResponse, errorResponse } from '@/lib/api-response'
 import { NextRequest } from 'next/server'
 import { orderUpdateSchema } from '@/lib/validations/order'
