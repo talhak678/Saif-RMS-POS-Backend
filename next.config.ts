ffc0f98ef9d3fc7c4804d52780df71b22178e352
hasanhayat <aylskhsmvz.v@gmail.com>
2026-03-14 18:17:29 +0500

    ../

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   // Static headers removed to avoid conflicts with Middleware.
   // Middleware now handles CORS dynamically.
 };
